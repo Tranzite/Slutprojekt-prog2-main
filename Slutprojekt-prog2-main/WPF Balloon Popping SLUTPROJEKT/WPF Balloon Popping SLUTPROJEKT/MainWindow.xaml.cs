e61380b2501b8a0fce2613c624c436a6b06baf9b
Tranzite <54800477+Tranzite@users.noreply.github.com>
2021-05-27 13:22:31 +0200

    Factory method complete

--- Slutprojekt-prog2-main/WPF Balloon Popping SLUTPROJEKT/WPF Balloon Popping SLUTPROJEKT/MainWindow.xaml.cs
+++ Slutprojekt-prog2-main/WPF Balloon Popping SLUTPROJEKT/WPF Balloon Popping SLUTPROJEKT/MainWindow.xaml.cs
@@ -31,6 +31,9 @@ namespace WPF_Balloon_Popping_SLUTPROJEKT
 
         List<Rectangle> itemRemover = new List<Rectangle>(); // The balloons that are going to reach the top and no longer reachable are going to be added into this list and it will then remove it from the canvas
 
+        List<String> balloonTags = new List<String> { "B1", "B2", "B3", "B4", "B5" };
+        List<Balloons> balloonList = new List<Balloons>();
+
         ImageBrush backgroundImage = new ImageBrush(); // imports and add the background image(s)
 
         int balloonSkins;
@@ -76,7 +79,7 @@ namespace WPF_Balloon_Popping_SLUTPROJEKT
                     MyCanvas.Children.Remove(activeRec); // removes the balloon from the canvas
 
                     score += 1;
-                    if (score % 10 == 0) // every 7 popped balloons the speed will increase by 1. so speed will be added at 7,14,21 etc
+                    if (score % 10 == 0) // every 10 popped balloons the speed will increase by 1.
                     {
                         speed++;
                     }
@@ -104,6 +107,8 @@ namespace WPF_Balloon_Popping_SLUTPROJEKT
         }
         private void GameEngine(object sender, EventArgs e)
         {
+            
+
             scoreText.Content = "Score: " + score;
 
             intervals -= 10;
@@ -112,24 +117,25 @@ namespace WPF_Balloon_Popping_SLUTPROJEKT
             {
                 ImageBrush balloonImage = new ImageBrush();
 
-                balloonSkins += 1;
+                balloonSkins += 1; // this determines which balloon is gonna be sent, 1-5
 
 
                 if (balloonSkins > 5)
                 {
                     balloonSkins = 1;
                 }
-
-                Balloons baloon = BalloonFactory.GetBalloon(balloonSkins);
+                
+                Balloons balloon = BalloonFactory.GetBalloon(balloonSkins);
 
                 Rectangle newBalloon = new Rectangle // defines the balloons properties, height,width and the image through the balloonfactory
                 {
-                    Tag = baloon.BalloonType,
-                    Height = baloon.Height,
-                    Width = baloon.Width,
-                    Fill = baloon.BalloonImage,
-                    
+                    Tag = balloon.BalloonType,
+                    Height = balloon.Height,
+                    Width = balloon.Width,
+                    Fill = balloon.BalloonImage,
+
                 };
+                
 
                 Canvas.SetLeft(newBalloon, rand.Next(50, 400));
                 Canvas.SetTop(newBalloon, 600);
@@ -141,15 +147,51 @@ namespace WPF_Balloon_Popping_SLUTPROJEKT
 
             foreach (var x in MyCanvas.Children.OfType<Rectangle>())
             {
-                if ((string)x.Tag != "")
+                if (balloonTags.Contains(x.Tag))
                 {
-
                     i = rand.Next(-5, 5);
+                    switch (x.Tag)
+                    {
+                        case "B1" :
+                            var B1 = new BalloonOne();
+                            Canvas.SetTop(x, Canvas.GetTop(x) - B1.Speed); // This moves the balloon towards the top of the screen
 
-                    Canvas.SetTop(x, Canvas.GetTop(x) - speed); // This moves the balloon towards the top of the screen
+                            Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1)); // This moves the balloon side to side.
+                            break;
 
-                    Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1)); // This moves the balloon side to side.
+                        case "B2":
+                            var B2 = new BalloonTwo();
+                            Canvas.SetTop(x, Canvas.GetTop(x) - B2.Speed); // This moves the balloon towards the top of the screen
 
+                            Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1)); // This moves the balloon side to side.
+
+                            break;
+                        case "B3":
+                            var B3 = new BalloonThree();
+                            Canvas.SetTop(x, Canvas.GetTop(x) - B3.Speed); // This moves the balloon towards the top of the screen
+
+                            Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1)); // This moves the balloon side to side.
+
+                            break;
+                        case "B4":
+                            var B4 = new BalloonFour();
+                            Canvas.SetTop(x, Canvas.GetTop(x) - B4.Speed); // This moves the balloon towards the top of the screen
+
+                            Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1)); // This moves the balloon side to side.
+
+                            break;
+                        case "B5":
+                            var B5 = new BalloonFive();
+                            Canvas.SetTop(x, Canvas.GetTop(x) - B5.Speed); // This moves the balloon towards the top of the screen
+
+                            Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1)); // This moves the balloon side to side.
+
+                            break;
+
+
+                    }
+
+                    
 
                 }
                 
